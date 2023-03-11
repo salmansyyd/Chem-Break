b3e67c80f6f8e837682d331326cc1e1b5a800fa4
Sayyed-Salman <sayyedsalman2000@gmail.com>
2023-03-11 22:23:56 +0530

    Add Tests - Adds create new user test

--- tests/test_add_admin.py
+++ tests/test_add_admin.py
@@ -1,17 +1,23 @@
 from flask_testing import TestCase
-from app import create_app as app
+from app import create_app as app_factory
 from app import db
 from app.models import User
+import unittest
+from config import Config
+
+NEW_USERNAME = "salman"
+NEW_PASSWORD = "salman"
 
 
 class UserCreationTest(TestCase):
 
     def create_app(self):
+        app = app_factory(config=Config)
         db.create_all()
         return app
 
     def test_create_user(self):
-        user = User(username='test_user', password='test_password')
+        user = User(username=NEW_USERNAME, password=NEW_PASSWORD)
         db.session.add(user)
         db.session.commit()
 
@@ -19,3 +25,7 @@ class UserCreationTest(TestCase):
         self.assertIsNotNone(created_user)
         self.assertEqual(created_user.username, 'test_user')
         self.assertEqual(created_user.password, 'test_password')
+
+
+if __name__ == "__main__":
+    unittest.main()
